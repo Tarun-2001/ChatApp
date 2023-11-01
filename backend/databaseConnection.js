2e2e347bdebe36189ab51b256cddb3f4bcd42296
TarunChandra <tarunmanepally4@gmail.com>
2023-11-01 12:42:53 +0530

    Remove .env from tracking

--- backend/databaseConnection.js
+++ backend/databaseConnection.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose')
 
-const mongooseUrl = `mongodb+srv://kiran:database@cluster0.np63w4z.mongodb.net/`;
-
 const connectToDataBase = async ()=>{
     try{
-        await mongoose.connect(mongooseUrl,{
+        await mongoose.connect(process.env.MONGOURL,{
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         });
         console.log("Mongoodb connected successfully");
-
     }
     catch(error){
         console.log("Error while connecting database");
