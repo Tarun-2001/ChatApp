e06f6cc5f646c0f32f4829e331db05b8288bf32d
TarunChandra <tarunmanepally4@gmail.com>
2023-12-08 00:38:03 +0530

    Revert "completed search bar in UI" reverted This reverts commit 83c5375859da3885abffc8749cc7e46b07437d68.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Button } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import LoginPage from "./Components/LoginPage";
-import SignUp from "./Components/SignUp";
-import Navbar from "./Components/Navbar";
-import ChatState from "./Context/Chat/ChatState";
-import ChatPage from "./Components/ChatPage";
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import LoginPage from './Components/LoginPage';
+import SignUp from './Components/SignUp'
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div>
-      <ChatState>
-      {/* <Navbar></Navbar> */}
-      <ChatPage></ChatPage>
-        <Routes>
-          <Route exact path="/login" element={<LoginPage />} />
-          <Route exact path="/signup" element={<SignUp />} />
-        </Routes>
-      </ChatState>
-    </div>
+       <Navbar></Navbar>
+       
+    <Routes>
+      <Route exact path="/login" element={<LoginPage />} />
+      <Route exact path="/signup" element={<SignUp />} />   
+    </Routes>
+        </div>
   );
 }
 
